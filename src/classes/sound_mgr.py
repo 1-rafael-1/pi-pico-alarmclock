58fcea88bfd26158866a323506f1e585a77123a1
1-rafael-1 <rafaelkoch@outlook.de>
2024-05-11 23:34:45 +0200

    add new driver; does not help; begin preparing for hard power-down

--- src/classes/sound_mgr.py
+++ src/classes/sound_mgr.py
@@ -1,28 +1,25 @@
-from drivers.picodfplayer import DFPlayer
+from drivers.dfplayer_mini import DFPlayerMini
 from utime import sleep
 
 class SoundManager:
     def __init__(self, state_mgr):
-        self.player = DFPlayer(uartInstance=0, txPin=0, rxPin=1, busyPin=27)
+        self.player = DFPlayerMini(uartinstance=0, tx_pin=0, rx_pin=1, power_pin=16)
         self.state_mgr = state_mgr
 
-    def initialize(self):
-        self.standby()
-
-    def standby(self):
-        self.player.standby()
+    def delay(self):
+        self.player.begin()
 
     def reset(self):
         self.player.reset()
 
-    def normal_mode(self):
-        self.player.normalWorking()
+    def set_eq(self, eq):
+        self.player.set_equalizer(eq)
 
-    def set_playback_mode(self, mode):
-        self.player.setPlaybackMode(mode)
+    def set_volume(self, volume):
+        self.player.set_volume(volume)
 
-    def play(self, folder, track):
-        self.player.playTrack(folder=folder, file=track)
+    def play(self, track):
+        self.player.play_track(track)
 
     def pause(self):
         self.player.pause()
@@ -30,27 +27,44 @@ class SoundManager:
     def resume(self):
         self.player.resume()
 
-    def set_eq(self, eq):
-        self.player.setEQ(eq)
+    def stop(self):
+        self.player.stop()
 
-    def set_volume(self, volume):
-        self.player.setVolume(volume)
+    def standby(self):
+        self.player.sleep()
 
-    def query_busy(self):
-        return self.player.queryBusy()
-    
+    def wake_up(self):
+        self.player.wake_up()
+
+    def power_on(self):
+        self.player.power_on()
+
+    def power_off(self):
+        self.player.power_off()
+
     def alarm_sequence(self):
         print("Playing alarm sequence")
+        self.delay()
+        self.wake_up()
+        self.delay()
+        self.reset()
+        self.delay()
         self.set_eq(4) # classic
+        self.delay()
         self.set_volume(10) # medium volume
-        self.play(1, 1) # play track 1 from folder 1
-        sleep(3) # wait for track to start playing
+        self.delay()
+        self.play(1) 
+        self.delay()
 
     def alarm_stop(self):
         print("Stopping alarm sequence")
-        self.pause() # stop the alarm
+        self.delay()
+        self.stop()
+        self.delay()
+        self.standby()
 
     def deinit(self):
+        self.delay()
         self.standby()
 
 ## Mocks for testing
@@ -68,22 +82,53 @@ def sound_manager_runs():
     print("Test SoundManager")
     state_mgr = MockStateManager()
     sound_mgr = SoundManager(state_mgr)
-    sound_mgr.reset()
+    sound_mgr.delay()
     sound_mgr.set_volume(5)
     #[WHEN]: SoundManager plays a track
-    sound_mgr.play(1, 1)
+    print("Playing track 1")
+    sound_mgr.delay()
+    sound_mgr.play(1)
     #[THEN]: SoundManager is playing the track
     sleep(10)
+    print("pause")
     #[WHEN]: SoundManager pauses the track
+    sound_mgr.delay()
     sound_mgr.pause()
     #[THEN]: SoundManager is paused
     sleep(10)
     #[WHEN]: SoundManager resumes the track
+    print("resume")
+    sound_mgr.delay()
     sound_mgr.resume()
     #[THEN]: SoundManager is resumed
     sleep(10)
     #[TEARDOWN]: SoundManager stops the track
+    print("pause")
+    sound_mgr.delay()
     sound_mgr.pause()
+
+def sound_manager_can_play_before_and_after_standby():
+    #[GIVEN]: SoundManager instance
+    print("Test SoundManager standby")
+    state_mgr = MockStateManager()
+    sound_mgr = SoundManager(state_mgr)
+    #[WHEN]: SoundManager plays a track
+    print("Playing track 1")
+    sound_mgr.play(1)
+    #[THEN]: SoundManager is playing the track
+    sleep(10)
+    #[WHEN]: SoundManager goes to standby
+    print("standby")
+    sound_mgr.standby()
+    #[THEN]: SoundManager is stopped
+    sleep(10)
+    #[WHEN]: SoundManager plays a track
+    print("Playing track 1")
+    sound_mgr.play(1)
+    #[THEN]: SoundManager is playing the track
+    sleep(10)
+    #[TEARDOWN]: SoundManager stops the track
+    sound_mgr.standby()
 
 def sound_manager_alarm_sequence_can_start_and_stop():
     #[GIVEN]: SoundManager instance
@@ -107,7 +152,7 @@ def sound_does_not_play_when_device_has_idled_out():
     sleep(360)
     print("was idle for 6 minutes")
     #[THEN]: SoundManager is no longer playing the track
-    sound_mgr.play(1, 1)
+    sound_mgr.play(1)
     sleep(10)
 
 def sound_manager_can_be_woken_up():
@@ -116,16 +161,9 @@ def sound_manager_can_be_woken_up():
     state_mgr = MockStateManager()
     sound_mgr = SoundManager(state_mgr)
     #[WHEN]: SoundManager is woken up
-    sound_mgr.normal_mode()
+    sound_mgr.wake_up()
     #[THEN]: SoundManager is resumed
-    sound_mgr.play(1, 1)
+    sound_mgr.play(1)
     sleep(10)
     #[TEARDOWN]: SoundManager stops the track
     sound_mgr.standby()
-
-def run_tests():
-    sound_manager_runs()
-    sound_manager_alarm_sequence_can_start_and_stop()
-    sound_does_not_play_when_device_has_idled_out()
-    sound_manager_can_be_woken_up()
-    print("All tests passed")
