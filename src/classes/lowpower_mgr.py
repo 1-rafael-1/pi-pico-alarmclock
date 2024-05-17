179479bec450d86384dc26afb7408b0abdbbd470
rafael <rafael>
2024-05-17 12:49:28 +0200

    chore: Refactor LowPowerManager class to use instance variables and fix typo

--- src/classes/lowpower_mgr.py
+++ src/classes/lowpower_mgr.py
@@ -3,16 +3,16 @@ import drivers.lowpower as lowpower
 class LowPowerManager:
     def __init__(self, state_mgr):
         self.state_mgr = state_mgr
-        green_pin=state_mgr.button_green_pin()
-        blue_pin=state_mgr.button_blue_pin()
-        yellow_pin=state_mgr.button_yellow_pin()
-        is_lowpower_mode = False
+        self.green_pin=state_mgr.button_green_pin()
+        self.blue_pin=state_mgr.button_blue_pin()
+        self.yellow_pin=state_mgr.button_yellow_pin()
+        self.is_lowpower_mode = False
 
     def enter_lowpower_mode(self):
         self.state_mgr.deinit()
-        is_lowpower_mode = True
+        self.is_lowpower_mode = True
         print("Entering lowpower mode...")
-        print(f"Pins are: {green_pin}, {blue_pin}, {yellow_pin}")
-        lowpower.dormant_until_pins([green_pin, blue_pin, yellow_pin])
-        is_lowpower_mode = False
+        print(f"pins are: {self.green_pin}, {self.blue_pin}, {self.yellow_pin}")
+        lowpower.dormant_until_pins([self.green_pin, self.blue_pin, self.yellow_pin], edge=True, high=True)
+        self.is_lowpower_mode = False
         self.state_mgr.initialize()
