b91afab65d7196ace38f1469ba192e278e480fa7
rafael <rafael>
2024-05-20 22:36:54 +0200

    Update logging settings in ApplicationManager

--- src/classes/app_manager.py
+++ src/classes/app_manager.py
@@ -10,11 +10,11 @@ class ApplicationManager:
 
     @micropython.native
     def set_logging_level(self):
-        self.state_mgr.log_set_verbose(True)
-        self.state_mgr.log_set_log(True)
+        self.state_mgr.log_set_verbose(False)
+        self.state_mgr.log_set_log(False)
         self.state_mgr.log_set_max_log_length(1000)
         self.state_mgr.log_set_log_file('log.txt')
-        self.state_mgr.log_set_clean_log(True)
+        self.state_mgr.log_set_clean_log(False)
 
     @micropython.native
     def initialize(self):
